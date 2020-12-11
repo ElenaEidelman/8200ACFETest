f897921645f3da1760efcb12533085f2f0df6d88
Alena <marlen8088@gmail.com>
2020-12-11 22:11:59 +0200

    edited test

--- public/filesJS/createJsonObj.js
+++ public/filesJS/createJsonObj.js
@@ -1,45 +1,45 @@
 
-class Person  {
-    constructor(fName,lName,age,gender,career,experience){
-        this.fName = fName;
-        this.lName = lName;
-        this.age = age;
-        this.gender = gender;
-        this.career = career;
-        this.experience = experience;
-    }
-}
+// class Person  {
+//     constructor(fName,lName,age,gender,career,experience){
+//         this.fName = fName;
+//         this.lName = lName;
+//         this.age = age;
+//         this.gender = gender;
+//         this.career = career;
+//         this.experience = experience;
+//     }
+// }
 
-//data hard coded
-let fnamesARR = ["James","John","Robert","Mary","Patricia","Jennifer","Michael","William","Elizabeth","Thomas","Jessica","Ryan","Cynthia","Kathleen","Aaron","Julie","Jerry"];
-let lnamesARR = ["SMITH","JOHNSON","WILLIAMS","JONES","BROWN","DAVIS","MILLER","WILSON","MOORE","TAYLOR","ANDERSON","THOMAS","JACKSON","WHITE","MARTINEZ","LEE","RODRIGUEZ"];
-let gendersARR = ["male","female"];
-let careersARR = ["Front End Developer","Senior Web Administrator","Senior Web Developer","Web Administrator","Web Developer","Webmaster","Application Developer","Applications Engineer","Associate Developer","Computer Programmer","Developer","Java Developer","Junior Software Engineer",".NET Developer","Senior Software Engineer","Senior System Architect","Senior System Designer","Software Architect","Software Developer","Systems Software Engineer"];
+// //data hard coded
+// let fnamesARR = ["James","John","Robert","Mary","Patricia","Jennifer","Michael","William","Elizabeth","Thomas","Jessica","Ryan","Cynthia","Kathleen","Aaron","Julie","Jerry"];
+// let lnamesARR = ["SMITH","JOHNSON","WILLIAMS","JONES","BROWN","DAVIS","MILLER","WILSON","MOORE","TAYLOR","ANDERSON","THOMAS","JACKSON","WHITE","MARTINEZ","LEE","RODRIGUEZ"];
+// let gendersARR = ["male","female"];
+// let careersARR = ["Front End Developer","Senior Web Administrator","Senior Web Developer","Web Administrator","Web Developer","Webmaster","Application Developer","Applications Engineer","Associate Developer","Computer Programmer","Developer","Java Developer","Junior Software Engineer",".NET Developer","Senior Software Engineer","Senior System Architect","Senior System Designer","Software Architect","Software Developer","Systems Software Engineer"];
 
-//function for create json data
-function createRandomObjects(fNames, lNames, genders, careers, objectCounts){
-    let count = 0;
-    let objectsJson = [];
-    let fName;
-    let lName;
-    let gender;
-    let career;
-    let age;
-    while(count < objectCounts){
-        //get random information from arrays
-        fName = fNames[Math.floor(Math.random() * fNames.length)];
-        lName = lNames[Math.floor(Math.random() * lNames.length)];
-        gender = genders[Math.floor(Math.random() * genders.length)];
-        age = Math.floor(Math.floor(Math.random() * (60 - 25) + 25));
-        career = careers[Math.floor(Math.random() * careers.length)];
-        experience = Math.floor(Math.floor(Math.random() * (age - 18)));
-        objectsJson[count] = new Person(fName,lName,age,gender,career,experience > 1 ? experience + ' years' : experience + ' year');
-        count++;
-    }
-    return JSON.stringify(objectsJson);
-}
+// //function for create json data
+// function createRandomObjects(fNames, lNames, genders, careers, objectCounts){
+//     let count = 0;
+//     let objectsJson = [];
+//     let fName;
+//     let lName;
+//     let gender;
+//     let career;
+//     let age;
+//     while(count < objectCounts){
+//         //get random information from arrays
+//         fName = fNames[Math.floor(Math.random() * fNames.length)];
+//         lName = lNames[Math.floor(Math.random() * lNames.length)];
+//         gender = genders[Math.floor(Math.random() * genders.length)];
+//         age = Math.floor(Math.floor(Math.random() * (60 - 25) + 25));
+//         career = careers[Math.floor(Math.random() * careers.length)];
+//         experience = Math.floor(Math.floor(Math.random() * (age - 18)));
+//         objectsJson[count] = new Person(fName,lName,age,gender,career,experience > 1 ? experience + ' years' : experience + ' year');
+//         count++;
+//     }
+//     return JSON.stringify(objectsJson);
+// }
 
-let objectsPeronsJson = '{"persons" :' + createRandomObjects(fnamesARR,lnamesARR,gendersARR,careersARR,2000) + ' }';
+// let objectsPeronsJson = '{"persons" :' + createRandomObjects(fnamesARR,lnamesARR,gendersARR,careersARR,2000) + ' }';
 //$.persons[?(@.fName.includes("ar"))]
 // $.persons[?(@.career.includes("Comp"))]
-console.log(objectsPeronsJson);
+//console.log(objectsPeronsJson);
--- public/filesJS/events.js
+++ public/filesJS/events.js
@@ -1,58 +1,123 @@
-function filterPersons(data){
-    let table = document.querySelector('div#personList > table');
-    table.innerHTML = ''; // empty table
-    
-    //start search after second inserted value
-    if(data.length >= 2){
-        let thElement;
-        let tdElement;
-        let persons = JSON.parse(objectsPeronsJson);
-        //filter
-        let result = persons.persons.filter(el => {
-            return el.career.toLowerCase().includes(data.toLowerCase());
-        });
-        result.forEach((searchResult, index) => {
-            let trElement = document.createElement('tr');
-            //create header of table and fill first line
-            if(index == 0){
-                let trElementHeader = document.createElement('tr');
-                let thIndexElement = document.createElement('th');
-                    thIndexElement.prepend('Id');
-                    trElementHeader.prepend(thIndexElement);
+let jsonFileContent = '';
+let searchingResult = [];
 
-                for (const key in searchResult) {
-                    thElement = document.createElement('th');
-                    thElement.append(key);
-                    trElementHeader.append(thElement);
-    
-                    tdElement = document.createElement('td');
-                    tdElement.append(searchResult[key]);
-                    trElement.append(tdElement);
-                }
-                
-                //create index of first row
-                let tdIndexElement = document.createElement('td');
-                tdIndexElement.prepend(index);
-                trElement.prepend(tdIndexElement);
+//import and display json file
+function importFile() {
+    let fileJson = document.getElementById('selectFiles').files;
+    let resultTextArea = document.getElementById('result');
+    if (fileJson.length <= 0 || !fileJson[0].type.includes('json')) {
+        return false;
+    }
+    //if file not empty
+    else{
+        resultTextArea.style.height = "100px";
+        let fileReader = new FileReader();
+        fileReader.readAsText(fileJson.item(0));
+        fileReader.onload = function(e) { 
+          jsonFileContent = JSON.parse(e.target.result);
+          let formatted = JSON.stringify(jsonFileContent,null,2);
+      
+          resultTextArea.style.height = "300px";
+          resultTextArea.value = formatted;
+        }
+    }
+};
 
-                table.append(trElementHeader);
-                table.append(trElement);
-            }
-            //fill table
-            else{
-                for (const key in searchResult) {
-                    tdElement = document.createElement('td');
-                    tdElement.append(searchResult[key]);
-                    trElement.append(tdElement);
-                    
-                }
-                //create index of row
-                let tdIndexElement = document.createElement('td');
-                tdIndexElement.append(index);
-                trElement.prepend(tdIndexElement);
-                table.append(trElement);
-            }
-        });
+function filterJsonFile(dataSearch){
+    searchingResult = [];
+    let resultTextArea = document.getElementById('result');
+
+//start filter after third inserted key
+    if(dataSearch.length > 2){
+        let formattedJson = toJsonFormat(jsonFileContent, dataSearch);
+        resultTextArea.value = JSON.stringify(formattedJson,null,2);
+    }
+    //reset text area
+    else{
+        resultTextArea.value = JSON.stringify(jsonFileContent,null,2);
     }
 }
+
+//create new object after filtered data in json format
+function toJsonFormat(theObject, keyValue){
+    //debugger
+    var result = null;
+    if(theObject instanceof Array) {
+        for(var i = 0; i < theObject.length; i++) {
+            result = toJsonFormat(theObject[i], keyValue);
+            //debugger
+        }
+    }
+    else
+    {
+        for(var prop in theObject) {
+            if(prop.includes(keyValue)) {
+                if(typeof theObject[prop] == 'object'){
+                    let tempObj = {};
+                    tempObj[prop] = theObject[prop];
+                    searchingResult.push(tempObj);
+                }
+                else{
+                    searchingResult.push(prop + ' : ' + theObject[prop]);
+                }
+            }
+            if(theObject[prop] instanceof Object || theObject[prop] instanceof Array)
+                result = toJsonFormat(theObject[prop], keyValue);
+        }
+    }
+
+    return  searchingResult;
+}
+
+//not for use yet
+// function displayData(data){
+//     let table = document.querySelector('div#jsonList > table');
+//     table.innerHTML = ''; // empty table
+    
+//     data.forEach((searchResult, index) => {
+//         let trElement = document.createElement('tr');
+//         //create header of table and fill first line
+//         if(index == 0){
+//             let trElementHeader = document.createElement('tr');
+//             let thIndexElement = document.createElement('th');
+//                 thIndexElement.prepend('Id');
+//                 trElementHeader.prepend(thIndexElement);
+
+//             for (const key in searchResult) {
+//                 thElement = document.createElement('th');
+//                 thElement.append(key);
+//                 trElementHeader.append(thElement);
+
+//                 tdElement = document.createElement('td');
+//                 tdElement.append(searchResult[key]);
+//                 trElement.append(tdElement);
+//             }
+            
+//             //create index of first row
+//             let tdIndexElement = document.createElement('td');
+//             tdIndexElement.prepend(index);
+//             trElement.prepend(tdIndexElement);
+
+//             table.append(trElementHeader);
+//             table.append(trElement);
+//         }
+//         //fill table
+//         else{
+//             for (const key in searchResult) {
+//                 tdElement = document.createElement('td');
+//                 tdElement.append(searchResult[key]);
+//                 trElement.append(tdElement);
+                
+//             }
+//             //create index of row
+//             let tdIndexElement = document.createElement('td');
+//             tdIndexElement.append(index);
+//             trElement.prepend(tdIndexElement);
+//             table.append(trElement);
+//         }
+//     });
+// }
+
+
+
 
